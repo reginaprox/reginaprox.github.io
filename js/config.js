431c15e2d78a1221f90c7ad207f275ecfce302bb
reginaprox <55797817+reginaprox@users.noreply.github.com>
2021-11-23 05:17:08 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -123,7 +123,7 @@ var l_musicRandom = false;
 
 // Music volume
 // Choose a value between 0 (quietest) and 100 (loudest)
-var l_musicVolume = 10;
+var l_musicVolume = 4;
 
 // Enable custom messages?
 // 'true' or 'false'
