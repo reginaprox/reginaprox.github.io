bf0b2739b8ca9a299f3f465bb674ff0e147d35ed
reginaprox <55797817+reginaprox@users.noreply.github.com>
2022-08-01 09:37:13 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -120,7 +120,7 @@ var l_musicDisplay = true;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{youtube: "https://youtu.be/redlketVxn4?list=LL", name: "Carpenter Brut - Inferno Galore"},
+	{youtube: "", name: "Carpenter Brut - Inferno Galore"},
 ];
 
 // Random music order?
